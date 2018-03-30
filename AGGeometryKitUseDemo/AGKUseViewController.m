5f912198b7f812c34961ff1e3ccdbf4e15f3af60
fhc <fu_huicong@qq.com>
2018-03-30 17:24:58 +0800

    四个角拖动+手势

--- AGGeometryKitUseDemo/AGKUseViewController.m
+++ AGGeometryKitUseDemo/AGKUseViewController.m
@@ -9,6 +9,9 @@
 #import "AGKUseViewController.h"
 #import "AGGeometryKit.h"
 
+static const CGFloat kMaxScale = 2.0; //最大缩放比例
+static const CGFloat kMinScale = 0.5; //最小缩放比例
+
 @interface AGKUseViewController ()<UIGestureRecognizerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *tlControl;
@@ -29,13 +32,15 @@
 @property (nonatomic, assign) CGPoint lastTranslation;
 
 
-
 @end
 
 @implementation AGKUseViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString * uuid = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    NSLog(@"%@",uuid);
     
     [self initialData];
 }
@@ -106,13 +111,65 @@
     [self handleRecognizers:recognizer];
 }
 
-- (void)handleRecognizers:(UIGestureRecognizer *)rcongnizer {
+- (void)handleRecognizers:(UIGestureRecognizer *)recognizer {
     
+    if (AGKQuadEqual(self.initialQuad, AGKQuadZero)  && CGPointEqualToPoint(_initialTouchLocation, CGPointZero)) {
+        _initialQuad = self.targetView.layer.quadrilateral;
+        _initialTouchLocation = [recognizer locationInView:self.view];
+        _lastTranslation = CGPointZero;
+        _lastRotation = 0.0;
+        _lastScale = 1.0;
+    }
+    
+    if ([self isActive:_pinchRecognizer]) {
+        
+        _lastScale = [_pinchRecognizer scale];
+    }
+    
+    if ([self isActive:_rotationRecognizer]) {
+        _lastRotation = _rotationRecognizer.rotation;
+    }
+    
+    if ([self isActive:_panRecognizer]) {
+        _lastTranslation = [_panRecognizer translationInView:self.view];
+    }
+    
+    if (!AGKQuadEqual(self.initialQuad, AGKQuadZero)  && !CGPointEqualToPoint(_initialTouchLocation, CGPointZero)) {
+        
+        CGAffineTransform transform = CGAffineTransformIdentity;
+        transform = CGAffineTransformTranslate(transform, _initialTouchLocation.x, _initialTouchLocation.y);
+        transform = CGAffineTransformTranslate(transform, _lastTranslation.x, _lastTranslation.y);
+        transform = CGAffineTransformScale(transform, _lastScale, _lastScale);//_lastScale
+        transform = CGAffineTransformRotate(transform, _lastRotation);
+        transform = CGAffineTransformTranslate(transform, -_initialTouchLocation.x, -_initialTouchLocation.y);
+        
+        AGKQuad quad = AGKQuadApplyCGAffineTransform(_initialQuad, transform);
+        
+        _targetView.layer.quadrilateral = quad;
+        
+        [self updateControlPoints];
+    }
+    
+    if (![self isActive:_pinchRecognizer] && ![self isActive:_rotationRecognizer] && ![self isActive:_panRecognizer]) {
+        _initialQuad = AGKQuadZero;
+        _initialTouchLocation = CGPointZero;
+    }
+}
+
+- (BOOL)isActive:(UIGestureRecognizer *)recognizer {
+    return recognizer.state == UIGestureRecognizerStateBegan || recognizer.state == UIGestureRecognizerStateChanged;
 }
 
 - (void)updateQuadrilateralByControlPoints {
     AGKQuad quad = AGKQuadMake(self.tlControl.center, self.trControl.center, self.brControl.center, self.blControl.center);
     self.targetView.layer.quadrilateral = quad;
+}
+
+- (void)updateControlPoints {
+    _tlControl.center = _targetView.layer.quadrilateral.tl;
+    _trControl.center = _targetView.layer.quadrilateral.tr;
+    _brControl.center = _targetView.layer.quadrilateral.br;
+    _blControl.center = _targetView.layer.quadrilateral.bl;
 }
 
 - (void)didReceiveMemoryWarning {
